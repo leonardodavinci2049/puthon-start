3fa9cc4d49fdb8542e298b1813c04a960c4ec126
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2024-05-02 19:05:12 -0300

    First Commit

---
+++ aula01.py
@@ -0,0 +1,16 @@
+# Primeira aula de python
+print("Minha primeira aula de python")
+print('Hello World!')
+
+"""
+Docstring:  Significa string de documentação
+este é um comentário de várias linhas
+em python
+"""
+
+
+# Imprime um número
+# print(123) 
+""" Estou adorando Python """
+print('Agora sou dev.')
+#
